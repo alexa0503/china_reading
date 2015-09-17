ad9d304bde72cf302595d8aabfc3c268d5161212
alexa0503 <lori.w@live.cn>
2015-09-18 02:06:49 +0800

    数据库生成

--- src/AppBundle/Entity/Info.php
+++ src/AppBundle/Entity/Info.php
@@ -44,4 +44,152 @@ class Info
      * @ORM\Column(name="create_ip", type="string", length=60)
      */
     private $createIp;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return Info
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Info
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set job
+     *
+     * @param string $job
+     * @return Info
+     */
+    public function setJob($job)
+    {
+        $this->job = $job;
+
+        return $this;
+    }
+
+    /**
+     * Get job
+     *
+     * @return string 
+     */
+    public function getJob()
+    {
+        return $this->job;
+    }
+
+    /**
+     * Set mobile
+     *
+     * @param string $mobile
+     * @return Info
+     */
+    public function setMobile($mobile)
+    {
+        $this->mobile = $mobile;
+
+        return $this;
+    }
+
+    /**
+     * Get mobile
+     *
+     * @return string 
+     */
+    public function getMobile()
+    {
+        return $this->mobile;
+    }
+
+    /**
+     * Set createTime
+     *
+     * @param \DateTime $createTime
+     * @return Info
+     */
+    public function setCreateTime($createTime)
+    {
+        $this->createTime = $createTime;
+
+        return $this;
+    }
+
+    /**
+     * Get createTime
+     *
+     * @return \DateTime 
+     */
+    public function getCreateTime()
+    {
+        return $this->createTime;
+    }
+
+    /**
+     * Set createIp
+     *
+     * @param string $createIp
+     * @return Info
+     */
+    public function setCreateIp($createIp)
+    {
+        $this->createIp = $createIp;
+
+        return $this;
+    }
+
+    /**
+     * Get createIp
+     *
+     * @return string 
+     */
+    public function getCreateIp()
+    {
+        return $this->createIp;
+    }
 }
